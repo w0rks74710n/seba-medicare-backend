fd5101849a5df0c987d672937a1c2a4cf692e633
Marco Salazar <marco.salazar@tum.de>
2018-05-27 12:08:54 +0200

    Fix routes order so that not all requests are redirected to /

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@
 const express  = require('express');
 const router   = express.Router();
 
+router.use('/api', require('./api'));
 router.use('/', function(req, res) {
     res.send('<html><body><h1>Welcome to MediCare!</h1></body></html>');
 });
-router.use('/api', require('./api'));
 
 module.exports = router;
