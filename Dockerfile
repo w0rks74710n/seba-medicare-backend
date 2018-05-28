3b5df19cf71a7cd17e44bcce8ffdaaa1b4ea0cbe
Marco Salazar <marco.salazar@tum.de>
2018-05-28 16:49:35 +0200

    Reconfigure application port to 3001 so that frontend app can use 3000. Fix docker-compose to export new port

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN npm install
 
 COPY . /usr/src/app
 
-EXPOSE 3000
+EXPOSE 3001
 
 CMD [ "npm", "start" ]
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //Configuration variables
-const port     = process.env.PORT || '3000';
+const port     = process.env.PORT || '3001';
 const mongoURI = process.env.MONGODB_URI || 'mongodb://mongo:27017/medicare';
 
 module.exports = {
