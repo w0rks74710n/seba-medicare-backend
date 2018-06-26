f69d384be4b234592c39721dcebbd51825fb5b65
Marco Salazar <marco.salazar@tum.de>
2018-06-27 01:12:18 +0200

    Add registration and login to doctor controller for API (Modify previous implementation). Add endpoints for doctor login and registration.

--- src/controllers/doctor.js
+++ src/controllers/doctor.js
@@ -1,60 +1,80 @@
 "use strict";
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+const config = require('../config');
 const DoctorModel = require('../models/doctor');
 const DoctorProfileInformationModel = require('../models/doctorProfileInformation');
 
-const create = (req, res) => {
-
+const register = (req, res) => {
   if (validateCreateRequest(req, res)) {
-    const doctor = Object.assign(req.body);
+    const doctor = Object.assign(req.body, {password: bcrypt.hashSync(req.body.password, 8)});
 
     DoctorModel.create(doctor).then(doctor => {
-        const doctorProfileInformation =
-          new DoctorProfileInformationModel({doctor_id: doctor._id,
-            about: {},
-            contactInformation: {},
-            socialMedia: {},
-            experience: {},
-            education: {},
-            services: {},
-            images: {}
-          });
+      const doctorProfileInformation =
+        new DoctorProfileInformationModel({doctor_id: doctor._id,
+          about: {},
+          contactInformation: {},
+          socialMedia: {},
+          experience: {},
+          education: {},
+          services: {},
+          images: {}
+        });
 
-        DoctorProfileInformationModel.create(doctorProfileInformation);
-        res.status(200).json({successfullyCreated: 'Model', doctorId: doctor._id});
-      }).catch(error => {
-        if(error.code == 11000) {
-          res.status(400).json({
-            error: 'User exists',
-            message: error.message
-          })
-        } else {
-          res.status(500).json({
-            error: 'Internal server error',
-            message: error.message
-          })
-        }
+      DoctorProfileInformationModel.create(doctorProfileInformation);
+
+      const token = jwt.sign({ id: doctor._id, username: doctor.username }, config.JwtSecret, {
+        expiresIn: 86400
       });
+
+      res.status(200).json({token: token});
+    }).catch(error => {
+      if(error.code == 11000) {
+        res.status(400).json({
+          error: 'User exists',
+          message: error.message
+        })
+      } else {
+        res.status(500).json({
+          error: 'Internal server error',
+          message: error.message
+        })
+      }
+    });
   }
 };
 
-const getById = (req, res) => {
-  if (!Object.prototype.hasOwnProperty.call(req.params, 'doctor_id')) return res.status(400).json({
+const login = (req,res) => {
+  if (!Object.prototype.hasOwnProperty.call(req.body, 'password')) return res.status(400).json({
     error: 'Bad Request',
-    message: 'The request parameters must contain a doctor_id'
+    message: 'The request body must contain a password property'
   });
 
-  DoctorModel.findOne({ _id: req.params.doctor_id }).exec(function(error, doctor) {
-    if (!error) {
-      res.status(200).json({doctor});
-    } else {
-      res.status(400).json({
-        error: error.message,
-        message: 'This doctor does not exist! Please verify.'
+  if (!Object.prototype.hasOwnProperty.call(req.body, 'username')) return res.status(400).json({
+    error: 'Bad Request',
+    message: 'The request body must contain a username property'
+  });
+
+  DoctorModel.findOne({username: req.body.username}).exec()
+    .then((doctor) => {
+
+      // check if the password is valid
+      const isPasswordValid = bcrypt.compareSync(req.body.password, doctor.password);
+      if (!isPasswordValid) return res.status(401).send({token: null });
+
+      // if user is found and password is valid
+      // create a token
+      const token = jwt.sign({ id: doctor._id, username: doctor.username }, config.JwtSecret, {
+        expiresIn: 86400
       });
-    }
-  });
 
+      res.status(200).json({token: token});
+    })
+    .catch(error => res.status(404).json({
+      error: 'User Not Found',
+      message: error.message
+    }));
 };
 
 function validateCreateRequest(req, res) {
@@ -82,6 +102,6 @@ function validateCreateRequest(req, res) {
 }
 
 module.exports = {
-  create,
-  getById
+  register,
+  login
 };
--- src/routes/api/doctor.js
+++ src/routes/api/doctor.js
@@ -5,7 +5,7 @@ const router   = express.Router();
 
 const DoctorController = require('../../controllers/doctor');
 
-router.get('/:doctor_id', DoctorController.getById);
-router.post('/', DoctorController.create);
+router.post('/register', DoctorController.register);
+router.post('/login', DoctorController.login);
 
 module.exports = router;
